982092ef0de8143eab1e57754eeaa8a75c35585f
ysaito <>
2019-11-23 16:42:03 +0900

    Layout the record content

--- src/js/util/router.js
+++ src/js/util/router.js
@@ -2,18 +2,23 @@ import {Router} from 'backbone';
 
 import {menuView} from '../pages/menu';
 import {homeView} from '../pages/home';
+import {recordView} from '../pages/record';
 
 const AppRouter = Router.extend({
 	routes: {
 		'': 'begin',
-		'home': 'home'
+		'home': 'moveHome',
+		'record': 'moveRecord'
 	},
 	begin: function () {
 		menuView.render();
-		this.home();
+		this.moveRecord();
 	},
-	home: function () {
+	moveHome: function () {
 		homeView.render();
+	},
+	moveRecord: function () {
+		recordView.render();
 	}
 });
 

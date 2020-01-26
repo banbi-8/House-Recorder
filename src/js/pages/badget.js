c1756e1e0620215ed59ad03ee5b7b016541ee1e2
ysaito <>
2020-01-26 11:13:01 +0900

    In progress...

--- src/js/pages/badget.js
+++ src/js/pages/badget.js
@@ -1,22 +1,50 @@
 define([
 	'jquery',
 	'backbone',
-	'text!templates/badget.template'
+	'pages/dselector',
+	'pages/dialog',
+	'text!templates/badget.template',
+	// no var
+	'bootstrap',
+	'datatables'
 ], function (
 	$,
 	Backbone,
-	template
+	dSelector,
+	Dialog,
+	template,
 ) {
 return BadgetView = Backbone.View.extend({
 	el: '.contents-area',
 	template_: null,
 	initialize: function() {
+		this.dSelector_ = new dSelector();
+		this.dialog_ = new Dialog();
 		this.template_ = template;
 	},
 	events: {
+		'click .badget-content #test': 'addNewBadgetItem'
 	},
+	// public
 	render: function () {
 		this.$el.append(this.template_);
+		this.dSelector_.render();
+		$('#badget-table').DataTable({
+			autoFill: true
+		});
+	},
+	
+	// for events
+	addNewBadgetItem: function () {
+		$('#MODAL1').modal('show');
+	},
+
+	// private
+	getInputValues: function () {
+		let values = {};
+		values.bIncome = income = Number($('#income-input').val());
+		
+		return values;
 	}
 });
 });

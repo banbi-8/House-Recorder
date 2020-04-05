4b5b7a15901f4006ac25832beb4894152e7d359a
ysaito <>
2020-04-05 21:21:32 +0900

    Modify declaration

--- src/js/common/mediator.js
+++ src/js/common/mediator.js
@@ -1,22 +1,18 @@
 define([
-	'jquery',
-	'underscore',
-	'backbone'
 ],
 function (
-	$,
-	_,
-	Backbone
 ) {
-Mediator = Backbone.View.extend({
-	views: {},
-	addView: function (name, view) {
+class Mediator {
+	constructor () {
+		this.views = {};
+	}
+	addView (name, view) {
 		this.views[name] = view;
-	},
-	send: function (event, to, opt_data) {
+	}
+	send (event, to, opt_data) {
 		this.views[to].receive(event, opt_data);
 	}
-});
+};
 
 return new Mediator();
 });
